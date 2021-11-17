5e884263d811b9b7f5129790586c795059753d93
Mr-coder-Tee <tebatsoterrence.manaka@gmail.com>
2021-11-17 12:01:11 +0200

    front end

--- src/Components/AddAdmin/Addadmin.jsx
+++ src/Components/AddAdmin/Addadmin.jsx
@@ -1,59 +1,159 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import HotelsCard from "../Card/HotelsCard";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSearch, faEye, faEyeSlash, faAt, faLock } from "@fortawesome/free-solid-svg-icons";
+import {
+  faSearch,
+  faEye,
+  faEyeSlash,
+  faAt,
+  faLock,
+  faAngleLeft,
+  faAngleRight,
+} from "@fortawesome/free-solid-svg-icons";
+
+const hotels = [
+  { id: 1, name: "hotel 1" },
+  { id: 2, name: "hotel 2" },
+  { id: 3, name: "hotel 3" },
+  { id: 4, name: "hotel 4" },
+  { id: 5, name: "hotel 5" },
+  { id: 6, name: "hotel 6" },
+  { id: 7, name: "hotel 7" },
+  { id: 8, name: "hotel 8" },
+  { id: 9, name: "hotel 9" },
+  { id: 10, name: "hotel 10" },
+  { id: 11, name: "hotel 11" },
+];
 
 const AddAdmin = () => {
-    const [type, setType] = useState('password');
+  const [type, setType] = useState("password");
+  const [hotelSlice, setHotelSlice] = useState([]);
+  const [from, setFrom] = useState(0);
+  const [to, setTo] = useState(5);
+  const [prevBtn, setPrevBtn] = useState(true);
+  const [nextBtn, setNextBtn] = useState(false);
 
-    const EyeChanger = () => (
-        type === 'password' ? <FontAwesomeIcon icon={faEye} /> : <FontAwesomeIcon icon={faEyeSlash} />
-    )
-    const passwordVisibility = () => {
-        if (type === 'password') {
-            setType('text')
-        } else {
-            setType('password')
-        }
+  const EyeChanger = () =>
+    type === "password" ? (
+      <FontAwesomeIcon icon={faEye} />
+    ) : (
+      <FontAwesomeIcon icon={faEyeSlash} />
+    );
+  const passwordVisibility = () => {
+    if (type === "password") {
+      setType("text");
+    } else {
+      setType("password");
     }
+  };
 
+  const getSlice = () => {
+    const data = [];
+    if (hotels.length > 5) {
+      hotels.slice(from, to).map((item) => {
+        data.push(item);
+      });
+      setHotelSlice(data);
+    } else {
+      setHotelSlice();
+    }
+  };
 
-    return (<div className={`taps addAdminContainer`}>
-        <div className="searchBar">
-            <div className="searchInputContainer">
-                <FontAwesomeIcon icon={faSearch} className="icon" />
-                <input placeholder='search' className={`inputs`} />
-            </div>
-        </div>
+  const next = () => {
+    const size = hotels.length - to-1;
+    if (from > 5) {
+      setTo(to + 5);
+    } else {
+      setTo(to + size);
+    }
+    setFrom(to);
+    setPrevBtn(false)
+    getSlice();
+  };
+  const prev = () => {
+      
+    const dis=to-from;
+    setTo(from);
+    if (dis === 0) {
+        setFrom(from-5)
+        console.log("from-to->", from, to,dis);
+    }else{
+        console.log("from -to", from, to,dis);
+    } 
+  };
+  const setValues = () => {
+    setFrom(0);
+    if (hotels.length > 5) {
+      setTo(5);
+    } else {
+      setTo(hotels.length - 1);
+    }
+  };
 
-        <div className="addForm">
-            <div className="inputHolder">
-                Email
-                <div className="textInputContainer">
-                    <div className="inputIcons">
-                        <FontAwesomeIcon icon={faAt} />
-                        <input placeholder="Email" className="inputs" type='email' />
-                    </div>
-                </div>
-            </div>
-            <div className="inputHolder marginLeft">
-                Password
-                <div className="textInputContainer">
-                    <div className="inputIcons">
-                        <FontAwesomeIcon icon={faLock} />
-                        <input placeholder="Password" className="inputs" type={type} />
-                        <button type="button" onClick={() => passwordVisibility()} className="passwordbtn">
-                            <EyeChanger />
-                        </button>
-                    </div>
-                </div>
-            </div>
-            <button className='addBtn' >Add Hotel Admin</button>
+  useEffect(() => {
+    setValues();
+    getSlice();
+  }, []);
+
+  return (
+    <div className={`taps addAdminContainer`}>
+      <div className="searchBar">
+        <div className="searchInputContainer">
+          <FontAwesomeIcon icon={faSearch} className="icon" />
+          <input placeholder="search" className={`inputs`} />
         </div>
-        <div className="hotelList">
-            <HotelsCard/>
+      </div>
+
+      <div className="addForm">
+        <div className="inputHolder">
+          <div className="textInputContainer">
+            <div className="inputIcons">
+              <FontAwesomeIcon icon={faAt} />
+              <input placeholder="Email" className="inputs" type="email" />
+            </div>
+          </div>
         </div>
-    </div>);
-}
+        <div className="inputHolder marginLeft">
+          <div className="textInputContainer">
+            <div className="inputIcons">
+              <FontAwesomeIcon icon={faLock} />
+              <input placeholder="Password" className="inputs" type={type} />
+              <button
+                type="button"
+                onClick={() => passwordVisibility()}
+                className="passwordbtn"
+              >
+                <EyeChanger />
+              </button>
+            </div>
+          </div>
+        </div>
+        <button className="addBtn">Add Hotel Admin</button>
+      </div>
+      <div className="hotelList">
+        {hotelSlice.map((hotel, index) => (
+          <HotelsCard hotel={hotel} index={index + 1} />
+        ))}
+      </div>
+      <div className="hotelNavigation">
+        <button
+          className={`navBtn  ${prevBtn ? "prevDis" : "prev"}`}
+          onClick={() => prev()}
+          disabled={prevBtn}
+        >
+          <FontAwesomeIcon icon={faAngleLeft} style={{ marginRight: 5 }} />
+          Prev
+        </button>
+        <button
+          className={`navBtn  ${nextBtn ? "nextDis" : "next"}`}
+          onClick={() => next()}
+        >
+          Next
+          <FontAwesomeIcon icon={faAngleRight} style={{ marginLeft: 5 }} />
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default AddAdmin;
--- src/Components/Card/HotelsCard.jsx
+++ src/Components/Card/HotelsCard.jsx
@@ -1,22 +1,31 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTrashAlt, faUnlockAlt, faUser } from "@fortawesome/free-solid-svg-icons";
+import {
+  faTrashAlt,
+  faUnlockAlt,
+  faUser,
+} from "@fortawesome/free-solid-svg-icons";
 
-
-const HotelsCard = () => {
-    return (
-        <div className="hotelTap">
-            <div className="hotelName">
-                <h3>1.</h3>
-                <FontAwesomeIcon icon={faUser} />
-                <h3> Hotel name</h3>
-            </div>
-            <div className="btnSetter">
-                <FontAwesomeIcon icon={faTrashAlt} className="deleteBtn"/>
-                <FontAwesomeIcon icon={faUnlockAlt} />
-            </div>
+const HotelsCard = ({hotel,index}) => {
+  return (
+    <div className="hotelTap">
+      {/* <h3>{index+". "}</h3> */}
+      <div className="hotelName">
+        <div className="profileIconContainer ma-5-right">
+          <FontAwesomeIcon icon={faUser} className="profieIcon" />
         </div>
-    );
-}
+        <h3> {hotel.name}</h3>
+      </div>
+      <div className="btnSetter">
+        <button className="btnHotel ">
+          <FontAwesomeIcon icon={faTrashAlt} className="deleteIcon" />
+        </button>
+        <button className="btnHotel">
+          <FontAwesomeIcon icon={faUnlockAlt} className="reload"/>
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default HotelsCard;
